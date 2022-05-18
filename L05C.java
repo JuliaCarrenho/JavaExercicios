ef196dfb647daf9c40d2911def8fe74f6f28381a
Júlia <104109245+JuliaCarrenho@users.noreply.github.com>
2022-05-18 20:30:23 -0300

    Create L05C.java

---
+++ L05C.java
@@ -0,0 +1,15 @@
+package apostilaManzono;
+
+import java.util.*;
+
+public class L05C {
+	public static void main (String[]args) {
+		Scanner sc = new Scanner (System.in);
+		int h = 0;
+		
+		for (int cont = 1; cont <=100; cont++) {
+			h += cont;
+		}
+		System.out.println(h);
+	}
+}
