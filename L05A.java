40529e2a56c2c87a1a58ee4f233ac81f85d40dfb
Júlia <104109245+JuliaCarrenho@users.noreply.github.com>
2022-05-18 20:29:06 -0300

    Create L05A.java

---
+++ L05A.java
@@ -0,0 +1,17 @@
+package apostilaManzono;
+
+import java.util.*;
+
+
+public class L05A {
+	public static void main (String[]args) {
+		Scanner sc = new Scanner (System.in);
+		int q = 0;
+		
+		for (int c = 15; c <=200; c++) {
+			q = c*c ;
+			System.out.println(c + "² =" + q);
+		}
+	}
+
+}
