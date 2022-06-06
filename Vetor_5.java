a4bd5db66e48edb7da77c37e7feb3d3b1c96eb8c
Júlia <104109245+JuliaCarrenho@users.noreply.github.com>
2022-06-06 19:08:08 -0300

    Create Vetor_5.java

---
+++ Vetor_5.java
@@ -0,0 +1,24 @@
+package apostilaVetor;
+
+import java.util.*;
+
+public class Exercicio_5 {
+	public static void main (String[]args) {
+		
+		int [] C = {-03,-13,-23,-33,-43,-53,-63,-73,-83,-93};
+		C [0] = 0;
+		C [1] = 0;
+		C [2] = 0;
+		C [3] = 0;
+		C [4] = 0;
+		C [5] = 0;
+		C [6] = 0;
+		C [7] = 0;
+		C [8] = 0;
+		C [9] = 0;
+		
+		System.out.println( "vetor " + C [5] + " modificado");
+		
+	}
+
+}
