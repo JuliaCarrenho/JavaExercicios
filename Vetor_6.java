7d7dab7b4d1a51dfd0644d65522223a696b22f3f
Júlia <104109245+JuliaCarrenho@users.noreply.github.com>
2022-06-06 19:08:38 -0300

    Create Vetor_6.java

---
+++ Vetor_6.java
@@ -0,0 +1,17 @@
+package apostilaVetor;
+
+import java.util.*;
+
+public class Exercicio_6 {
+	public static void main (String[]args) {
+		
+		int D [] = {1,2,3,4,5,6,7,8,9,10};
+		for(int E : D) {
+			for (E = D.length -1; E>= 0; E--) {
+				System.out.println( D[E]);
+					
+
+		}
+		}
+	}
+}
