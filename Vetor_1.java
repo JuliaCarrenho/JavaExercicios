1480c96b4b7fc50de169c9c06a3c4d9bc8284d18
Júlia <104109245+JuliaCarrenho@users.noreply.github.com>
2022-06-06 19:05:49 -0300

    Create Vetor_1.java

---
+++ Vetor_1.java
@@ -0,0 +1,13 @@
+package apostilaVetor;
+ import java.util.*;
+
+ public class Exercicio_1 {
+	 public static void main (String[]args) {
+		 
+		 int [] x = {03,13,23,33,43,53,63,73,83,93};
+		 System.out.println(x [5]);
+		 
+		 
+	 }
+
+}
