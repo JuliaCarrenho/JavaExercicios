586b64815ec5717b8c6a4df2e665a9b38ee162e2
Júlia <104109245+JuliaCarrenho@users.noreply.github.com>
2022-06-06 19:06:52 -0300

    Create Vetor_3.java

---
+++ Vetor_3.java
@@ -0,0 +1,17 @@
+package apostilaVetor;
+
+import java.util.*;
+
+public class Exercicio_3 {
+	public static void main(String[]args) {
+		int b [] = {1,2,3,4,5,6,7,8,9,10};
+	    for (int h : b) {
+	    	if (h%2==0) {
+	    	System.out.println(h + " par");
+	    } else {
+	    	System.out.println(h + " ímpar");
+	    }
+	}
+	}
+
+}
