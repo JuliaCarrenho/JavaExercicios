88440807e4160bc6fb862e1a49ea1486ec5c72b0
Júlia <104109245+JuliaCarrenho@users.noreply.github.com>
2022-06-06 19:10:07 -0300

    Create Vetor_7.java

---
+++ Vetor_7.java
@@ -0,0 +1,17 @@
+package apostilaVetor;
+
+import java.util.*;
+
+public class Exercicio_7 {
+	public static void main (String[]args) {
+		int [] X = {03,13,23,33,43,53,63,73,83,93};
+		 for (int Y :X) {
+		   if (Y %2  ==0) {
+			   System.out.println(Y + " par");
+		    } else {
+		    	System.out.println(Y + " ímpar");
+		    }
+		}
+		}
+
+	}
