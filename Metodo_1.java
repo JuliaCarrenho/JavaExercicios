3a31b29e1858896503da19daaaf680abbfb37e34
Júlia <104109245+JuliaCarrenho@users.noreply.github.com>
2022-06-06 19:13:37 -0300

    Create Metodo_1.java

---
+++ Metodo_1.java
@@ -0,0 +1,17 @@
+package exerciciosIniciais;
+
+import java.util.*;
+
+ 
+ public class Exercicio1 {
+		static int meuMetodo (int x, int y) {
+			return x + y ;
+			
+		}
+	 
+		 public static void main (String [] args) {
+		  int W  = meuMetodo (6,9);
+		  System.out.println(W);
+	
+	}
+	}
